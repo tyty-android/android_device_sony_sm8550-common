82ed9c3a1b267e462738af7d0a903096a762929d
onlyfish <linotfish@outlook.com>
2026-03-26 09:42:56 +0800

    sm8550-common: Compile HWUI for performance
    * Activates a few optimizing compiler flags for HWUI.
    * -fno-omit-frame-pointer, -marm, -mapcs

--- common.mk
+++ common.mk
@@ -171,6 +171,9 @@ PRODUCT_COPY_FILES += \
 # Graphics
 TARGET_USES_VULKAN = true
 
+# HWUI
+HWUI_COMPILE_FOR_PERF := true
+
 PRODUCT_COPY_FILES += \
     frameworks/native/data/etc/android.hardware.opengles.aep.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/android.hardware.opengles.aep.xml \
     frameworks/native/data/etc/android.hardware.vulkan.compute-0.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/android.hardware.vulkan.compute.xml \
