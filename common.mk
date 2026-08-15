3095b0eb7ad503b255a7059750db49c974906f72
Yuan Tong <tongyuan200097@gmail.com>
2026-08-15 15:14:44 +0800

    sm8550-common: Remove unnecessary attestation support

--- common.mk
+++ common.mk
@@ -226,8 +226,8 @@ PRODUCT_PACKAGES += \
     android.hardware.hardware_keystore_V2.xml
 
 PRODUCT_COPY_FILES += \
-    $(LOCAL_PATH)/permissions/android.hardware.keystore.app_attest_key.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/android.hardware.keystore.app_attest_key.xml \
-    $(LOCAL_PATH)/permissions/android.software.device_id_attestation.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/android.software.device_id_attestation.xml
+    frameworks/native/data/etc/android.hardware.keystore.app_attest_key.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/android.hardware.keystore.app_attest_key.xml \
+    frameworks/native/data/etc/android.software.device_id_attestation.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/android.software.device_id_attestation.xml
 
 # Lineage Health
 PRODUCT_PACKAGES += \
